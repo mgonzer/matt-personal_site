0ffa8d399acc1b12e214d451a8e0036500a454f3
Matthew Gonzer <matthewgonzer@gmail.com>
2017-10-04 16:29:50 -0600

    work on portfolio page

--- src/Portfolio.js
+++ src/Portfolio.js
@@ -7,26 +7,31 @@ import lunaPortal from './assets/lunap.png'
 class Portfolio extends Component {
   render() {
     return (
-      <div>
-        <div>
-          <div className="projectBox">
+      <div className="container">
+        <h2>Portfolio</h2>
+        <div className="projectsContainer">
+          <div className="projectBox2">
             <h3 className="project">LunaPortal</h3>
-            <p>RegXpress is an online tournament for developers to test their knowledge of regular expressions.</p>
+            <p>LunaPortal is practice management application built for an Acupuncture Clinic
+              that specializes in fertility and womans health.</p>
             <img className="image1" src={lunaPortal}/>
+
           </div>
+          <div className="projectBox">
             <h3 className="project">Little Helpers</h3>
             <p>Little Helpers is a task/reward tracker app for parents and their children.</p>
             <img className="image1" src={littlehelpers}></img>
-
-          <div className="projectBox">
+          </div>
+          <div className="projectBox2">
           <h3 className="project">RegXpress</h3>
           <p>RegXpress is an online tournament for developers to test their knowledge of regular expressions.</p>
           <img className="image1" src={regxpress}/>
           </div>
+          <div className="projectBox">
           <h3 className="project">Outside My Window</h3>
           <p>Outside my Window is an image based weather app that allows users to acually "see" what it looks like outside</p>
           <img className="image1" src={omw}></img>
-
+          </div>
         </div>
       </div>
 
